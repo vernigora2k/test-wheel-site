a0a417671ff2dae4e984b09159f3909219030ccf
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-10-14 00:20:30 +0300

    v1.0.6stable

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,11 @@
 const singInBtn = document.getElementById('sing-in-btn')
 const popupSingIn = document.querySelector('.popup-sing-in')
 const popupSingInClose = document.querySelector('.popup-sing-in__close-btn')
+const callMeBtn = document.querySelector('.call-me-btn')
+const popupCallMeBack = document.querySelector('.popup-call-me-back')
+const pupupCallMeBackClose = document.querySelector('.popup-call-me-back__close-btn')
+
+
 
 singInBtn.addEventListener('click', () => {
     popupSingIn.classList.toggle('active')
@@ -10,5 +15,12 @@ popupSingInClose.addEventListener('click', () => {
     popupSingIn.classList.toggle('active')
 })
 
+pupupCallMeBackClose.addEventListener('click', () => {
+    popupCallMeBack.classList.toggle('activ')
+})
+
+callMeBtn.addEventListener('click', () => {
+    popupCallMeBack.classList.toggle('activ')
+})
 
 
