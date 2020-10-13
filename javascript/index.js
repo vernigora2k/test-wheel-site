b3802a5d361ff42f5a7c5d44b10867f15ff53a60
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-10-13 22:24:32 +0300

    v1.0.5stable

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,14 @@
-let singInBtn = document.getElementById('sing-in-btn')
+const singInBtn = document.getElementById('sing-in-btn')
+const popupSingIn = document.querySelector('.popup-sing-in')
+const popupSingInClose = document.querySelector('.popup-sing-in__close-btn')
 
-singInBtn.onclick()
+singInBtn.addEventListener('click', () => {
+    popupSingIn.classList.toggle('active')
+})
+
+popupSingInClose.addEventListener('click', () => {
+    popupSingIn.classList.toggle('active')
+})
+
+
+
